c0ef25a934b3423ceb7bb4f277aadfe8b758de8a
Oliver Jarvis <oliversimonjarvis@gmail.com>
2023-03-01 22:12:30 +0100

    minimal run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+import Bigshorts.BackgroundVideo
+import Bigshorts.Generative
+import Bigshorts.PostRetrieval
+import Bigshorts.Uploader
+import Bigshorts.VideoEditing
+
+if __name__ == "__main__":
+    #add cli here
+    pass
