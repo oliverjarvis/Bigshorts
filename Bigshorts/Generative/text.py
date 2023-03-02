7225bd1c8c1b361ae4a4a7491e5e9d99a6545c86
Oliver Jarvis <oliversimonjarvis@gmail.com>
2023-03-02 21:05:09 +0100

    added structure

---
+++ Bigshorts/Generative/text.py
@@ -0,0 +1,6 @@
+class Text:
+    def __init__(self, config):
+        self.config = config
+    def generate(self, text):
+        # api call to generate
+        pass
--- run.py
+++ run.py
@@ -7,3 +7,15 @@ import Bigshorts.VideoEditing
 if __name__ == "__main__":
     #add cli here
     pass
+
+#generate video by the following steps
+
+#[] find posts or get post from image bank
+#[] render post
+#[] position post on portrait sized image
+#[] add step by step render
+#[] add background music
+#[] add background video
+#[] generate description
+#[] generate video
+#[] upload video
